f44b582f910709c33ba42a1007b7a33f7b9d27ad
matematysek <107941900+matematysek@users.noreply.github.com>
2023-02-17 13:11:27 +0100

    Update metal_workshop.py

--- src/industries/metal_workshop.py
+++ src/industries/metal_workshop.py
@@ -2,9 +2,17 @@ from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(
     id="metal_workshop",
-    accept_cargos_with_input_ratios=[("STEL", 6), ("RFPR", 2)],
+    accept_cargos_with_input_ratios=[
+    ("STSH", 2),
+    ("COAT", 2),
+    ("ZINC", 2),
+    ("STWR", 2),
+]
     combined_cargos_boost_prod=True,
-    prod_cargo_types_with_output_ratios=[("GOOD", 8)],
+    prod_cargo_types_with_output_ratios=[
+    ("GOOD", 4),
+    ("STSE", 4),  
+]
     prob_in_game="3",
     prob_map_gen="5",
     map_colour="166",

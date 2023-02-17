328b71fa7dd83cda43de8a413c9191c006d64ecf
matematysek <107941900+matematysek@users.noreply.github.com>
2023-02-17 14:29:21 +0100

    Update wire_and_section_mill.py

--- src/industries/wire_and_section_mill.py
+++ src/industries/wire_and_section_mill.py
@@ -2,7 +2,7 @@ from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(
     id="wire_and_section_mill",
-    accept_cargos_with_input_ratios=[("STCB", 3), ("SOAP", 1), ("ALUM", 2), ("COPR", 2)],
+    accept_cargos_with_input_ratios=[("STCB", 3), ("ALUM", 2), ("COPR", 2)],
     combined_cargos_boost_prod=True,
     prod_cargo_types_with_output_ratios=[
         ("STSE", 4),

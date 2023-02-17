07c51b9a096578afb7db79fb6fde9e51ca0ae1d7
matematysek <107941900+matematysek@users.noreply.github.com>
2023-02-17 14:07:03 +0100

    Update body_plant.py

--- src/industries/body_plant.py
+++ src/industries/body_plant.py
@@ -2,7 +2,7 @@ from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(
     id="body_plant",
-    accept_cargos_with_input_ratios=[("STSH", 4), ("COAT", 2), ("GLAS", 2)],
+    accept_cargos_with_input_ratios=[("STSH", 4), ("COAT", 2), ("GLAS", 2), ("ELEC"],
     combined_cargos_boost_prod=True,
     prod_cargo_types_with_output_ratios=[("VBOD", 8)],
     prob_in_game="3",

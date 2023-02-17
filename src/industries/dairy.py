9184c3d96b7ad6546535b3e265a1d436b404b1b4
matematysek <107941900+matematysek@users.noreply.github.com>
2023-02-17 15:07:14 +0100

    Update dairy.py

--- src/industries/dairy.py
+++ src/industries/dairy.py
@@ -2,7 +2,7 @@ from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(
     id="dairy",
-    accept_cargos_with_input_ratios=[("MILK", 6)],
+    accept_cargos_with_input_ratios=[("MILK", 6), ("PACK", 1)],
     combined_cargos_boost_prod=True,
     prod_cargo_types_with_output_ratios=[("FOOD", 8)],
     prob_in_game="3",

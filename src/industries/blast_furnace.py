059c39d069b9cdcc21f413e5c945a3a80bc9c984
matematysek <107941900+matematysek@users.noreply.github.com>
2023-02-17 14:50:32 +0100

    Update blast_furnace.py

--- src/industries/blast_furnace.py
+++ src/industries/blast_furnace.py
@@ -2,9 +2,9 @@ from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(
     id="blast_furnace",
-    accept_cargos_with_input_ratios=[("IORE", 3), ("COAL", 2), ("SCMT", 3)],
+    accept_cargos_with_input_ratios=[("IORE", 3), ("COKEL", 2), ("LIME", 2), ("SCMT", 3)],
     combined_cargos_boost_prod=True,
-    prod_cargo_types_with_output_ratios=[("STEL", 8)],
+    prod_cargo_types_with_output_ratios=[("STEL", 8), ("SLAG", 2)],
     prob_in_game="3",
     prob_map_gen="5",
     map_colour="10",

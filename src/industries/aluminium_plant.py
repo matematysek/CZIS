b7f9f99b79de539c6355890c11fe6e1f06e4e332
matematysek <107941900+matematysek@users.noreply.github.com>
2023-02-17 12:09:15 +0100

    Update aluminium_plant.py

--- src/industries/aluminium_plant.py
+++ src/industries/aluminium_plant.py
@@ -2,9 +2,9 @@ from industry import IndustrySecondary, TileLocationChecks
 
 industry = IndustrySecondary(
     id="aluminium_plant",
-    accept_cargos_with_input_ratios=[("ALO_", 8)],
+    accept_cargos_with_input_ratios=[("AORE", 8), ("ACID", 8), ("ELEC", 2)],
     combined_cargos_boost_prod=True,
-    prod_cargo_types_with_output_ratios=[("ALUM", 8)],
+    prod_cargo_types_with_output_ratios=[("ALUM", 8), ("SLAG, 8)],
     prob_in_game="3",
     prob_map_gen="5",
     map_colour="19",
@@ -17,15 +17,6 @@ industry = IndustrySecondary(
 industry.economy_variations['BETTER_LIVING_THROUGH_CHEMISTRY'].enabled = True
 
 industry.economy_variations["STEELTOWN"].enabled = True
-industry.economy_variations["STEELTOWN"].accept_cargos_with_input_ratios = [
-    ("ALO_", 4),
-    ("SCMT", 2),
-    ("COKE", 2),
-]
-industry.economy_variations["STEELTOWN"].prod_cargo_types_with_output_ratios = [
-    ("ALUM", 7),
-    ("SLAG", 1),
-]
 
 
 industry.add_tile(
